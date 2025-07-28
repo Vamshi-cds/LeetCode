0a1370e9b9349dfd3a5fae7c0d8e0379e0f0350f
Vamshi <vamshibus25@gmail.com>
2025-07-28 18:58:26 -0500

    Time: 18 ms (23.29%), Space: 48.1 MB (43.87%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> check=new HashMap<>();
+        for(String s : strs){
+            String aToZ=checkAZ(s);
+            List<String> group=check.getOrDefault(aToZ,new ArrayList<>());
+            group.add(s);
+            check.put(aToZ,group);
+            
+        }
+        List<List<String>> res=new ArrayList<>();
+        for(List<String> values: check.values()){
+            res.add(values);
+        }
+        return res;
+
+    }
+    public static String checkAZ(String s){
+        int[] az=new int[26];
+        for(char x: s.toCharArray()){
+            az[x-'a']++;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int count: az){
+            sb.append("#");
+            sb.append(count);
+        }
+        return sb.toString();
+    }
+}
