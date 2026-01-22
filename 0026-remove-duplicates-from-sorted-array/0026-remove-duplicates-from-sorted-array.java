f20581ce0eb78866026d7a371c4b5f7a16b4e631
Vamshi <vamshibus25@gmail.com>
2026-01-22 01:31:05 -0600

    Time: 8 ms (4.23%), Space: 47.1 MB (5.16%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,13 +1,21 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length==0) return 0;
-        int k=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[i-1]){
-                nums[k]=nums[i];
-                k++;
-            }
+        if (nums.length == 0) return 0;
+
+        // Use Java 8 streams to get distinct elements
+        int[] unique = Arrays.stream(nums)
+                             .distinct()
+                             .toArray();
+
+        // Copy back to original array (to simulate in-place behavior)
+        for (int i = 0; i < unique.length; i++) {
+            nums[i] = unique[i];
         }
-        return k;
+
+        // Return count of unique elements
+        return unique.length;
     }
 }
