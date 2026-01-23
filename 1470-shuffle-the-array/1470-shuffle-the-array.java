ab3a9dfb049d5663d2a9b25bba1b8d2668d8e11b
Vamshi <vamshibus25@gmail.com>
2026-01-23 12:02:05 -0600

    Time: 0 ms (100%), Space: 46.7 MB (29.6%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int result[]=new int[nums.length];
+        int j=0;
+        for(int i=0;i<n;i++){
+            result[j]=nums[i];
+            j++;
+            result[j]=nums[i+n];
+            j++;
+        }
+        return result;
+    }
+}
