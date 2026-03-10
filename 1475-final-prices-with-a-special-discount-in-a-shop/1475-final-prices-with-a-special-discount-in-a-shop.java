45a675ce36b7fec3134a67f6b027036bfb073aa7
Vamshi <vamshibus25@gmail.com>
2026-03-10 14:07:15 -0500

    Time: 4 ms (52.79%), Space: 45.6 MB (22.12%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n=prices.length;
+        int[] result=new int[n];
+        Stack<Integer> stack=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()>prices[i]){
+                stack.pop();
+            }
+            result[i]=stack.isEmpty() ? prices[i]:prices[i]-stack.peek();
+            stack.push(prices[i]);
+        }
+        return result;
+    }
+}
